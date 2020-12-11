63e8cd7013b3204f31cf9ab033206de5554ee2ba
sp <saiprasadreddy081@gmail.com>
2020-12-10 19:18:12 -0500

    NavigationGraphs

---
+++ app/src/main/java/com/android/openapi/ui/auth/LauncherFragment.kt
@@ -0,0 +1,57 @@
+package com.android.openapi.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.android.openapi.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
+
+// TODO: Rename parameter arguments, choose names that match
+
+class LauncherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // setting listeners to navgraphs
+        register.setOnClickListener{
+            navRegistration()
+        }
+
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        forgot_password.setOnClickListener{
+            navForgotPassword()
+        }
+
+        // to remove focus from the textview and adding it to random view in the launcher fragment layout
+        focusable_view.requestFocus()
+
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegistration() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+}
---
+++ app/src/main/java/com/android/openapi/ui/auth/ZZZZPlaceHolder.kt
@@ -0,0 +1,2 @@
+package com.android.openapi.ui.auth
+
