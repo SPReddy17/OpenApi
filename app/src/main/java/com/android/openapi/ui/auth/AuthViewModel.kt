eaf6db1a5716d987a92a4b70c654b7f2ddada731
sp <saiprasadreddy081@gmail.com>
2020-12-11 18:04:46 -0500

    RetrofitSetup

--- app/src/main/java/com/android/openapi/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/android/openapi/ui/auth/AuthViewModel.kt
@@ -1,7 +1,11 @@
 package com.android.openapi.ui.auth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.android.openapi.api.auth.network_responses.LoginResponse
+import com.android.openapi.api.auth.network_responses.RegistrationResponse
 import com.android.openapi.repository.auth.AuthRepository
+import com.android.openapi.util.GenericApiResponse
 import javax.inject.Inject
 
 class AuthViewModel
@@ -10,4 +14,19 @@ constructor(
     val authRepository: AuthRepository
 ):ViewModel(){
 
+
+    fun testLogin() : LiveData<GenericApiResponse<LoginResponse>>{
+        return authRepository.testLoginRequest(
+            "saiprasadreddy081@gmail.com",
+            "Chinna$17"
+        )
+    }
+    fun testRegistration() : LiveData<GenericApiResponse<RegistrationResponse>>{
+        return authRepository.testRegistrationRequest(
+            "saiprasadreddy081@gmail.com",
+            "sp",
+        "Chinna",
+        "Chinna"
+        )
+    }
 }
---
+++ app/src/main/java/com/android/openapi/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.android.openapi.util
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://open-api.xyz/api/"
+    }
+}
