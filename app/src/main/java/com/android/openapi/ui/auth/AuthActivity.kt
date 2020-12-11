d336d8ab38ad2be8d1e03cf9379ee119c97cb3ea
sp <saiprasadreddy081@gmail.com>
2020-12-10 19:18:59 -0500

    NavigationGraphs

---
+++ app/src/main/java/com/android/openapi/ui/auth/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.android.openapi.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.openapi.R
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
