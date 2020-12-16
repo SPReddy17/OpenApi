7535bca9b46bc3087542e070405d061811c5e451
sp <saiprasadreddy081@gmail.com>
2020-12-16 16:11:07 -0500

    NetworkBoundResource

--- app/src/main/java/com/android/openapi/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/android/openapi/ui/auth/AuthActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import com.android.openapi.R
 import com.android.openapi.ui.BaseActivity
 import com.android.openapi.ui.Response
@@ -14,7 +17,8 @@ import com.android.openapi.ui.main.MainActivity
 import com.android.openapi.viewmodels.ViewModelProviderFactory
 import javax.inject.Inject
 
-class AuthActivity : BaseActivity(){
+class AuthActivity : BaseActivity(),
+    NavController.OnDestinationChangedListener{
 
 
     @Inject
@@ -27,6 +31,7 @@ class AuthActivity : BaseActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
         viewModel = ViewModelProvider(this,providerFactory).get(AuthViewModel::class.java)
+        findNavController(R.id.auth_nav_host_fragment).addOnDestinationChangedListener(this)
         subscribeObservers()
 
     }
@@ -79,6 +84,14 @@ class AuthActivity : BaseActivity(){
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        viewModel.cancelActiveJobs()
     }
 
 }
