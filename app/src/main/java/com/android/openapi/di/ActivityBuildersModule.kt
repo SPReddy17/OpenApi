53b65f71f7b1d21a288702fa0dab533616996ab6
sp <saiprasadreddy081@gmail.com>
2021-01-04 13:45:30 -0500

    MAin Fragments and Layouts

--- app/src/main/java/com/android/openapi/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/android/openapi/di/ActivityBuildersModule.kt
@@ -4,6 +4,10 @@ import com.android.openapi.di.auth.AuthFragmentBuildersModule
 import com.android.openapi.di.auth.AuthModule
 import com.android.openapi.di.auth.AuthScope
 import com.android.openapi.di.auth.AuthViewModelModule
+import com.android.openapi.di.main.MainFragmentBuildersModule
+import com.android.openapi.di.main.MainModule
+import com.android.openapi.di.main.MainScope
+import com.android.openapi.di.main.MainViewModelModule
 import com.android.openapi.ui.auth.AuthActivity
 import com.android.openapi.ui.main.MainActivity
 import dagger.Module
@@ -19,7 +23,10 @@ abstract class ActivityBuildersModule {
     )
     abstract fun contributeAuthActivity(): AuthActivity
 
+    @MainScope
+    @ContributesAndroidInjector(
+        modules = [MainModule::class, MainFragmentBuildersModule::class, MainViewModelModule::class]
+    )
+    abstract fun contributeMainActivity(): MainActivity
 
-    @ContributesAndroidInjector
-    abstract  fun contributeMainActivity() :  MainActivity
 }
---
+++ app/src/main/java/com/android/openapi/ui/main/account/BaseAccountFragment.kt
@@ -0,0 +1,22 @@
+package com.android.openapi.ui.main.account
+
+import android.content.Context
+import android.util.Log
+import com.android.openapi.ui.DataStateChangeListener
+import dagger.android.support.DaggerFragment
+
+abstract class BaseAccountFragment : DaggerFragment(){
+
+    val TAG: String = "AppDebug"
+
+    lateinit var stateChangeListener: DataStateChangeListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try{
+            stateChangeListener = context as DataStateChangeListener
+        }catch(e: ClassCastException){
+            Log.e(TAG, "$context must implement DataStateChangeListener" )
+        }
+    }
+}
