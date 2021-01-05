e8eca2ee904822d139792b4f313f5e7a7540708e
sp <saiprasadreddy081@gmail.com>
2021-01-04 23:23:08 -0500

    CustomBottomNavController

--- app/src/main/java/com/android/openapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/android/openapi/ui/main/MainActivity.kt
@@ -4,27 +4,112 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import com.android.openapi.R
 import com.android.openapi.ui.BaseActivity
 import com.android.openapi.ui.auth.AuthActivity
-import kotlinx.android.synthetic.main.activity_main.progress_bar
+import com.android.openapi.ui.main.account.ChangePasswordFragment
+import com.android.openapi.ui.main.account.UpdateAccountFragment
+import com.android.openapi.ui.main.blog.UpdateBlogFragment
+import com.android.openapi.ui.main.blog.ViewBlogFragment
+import com.android.openapi.util.BottomNavController
+import com.android.openapi.util.setUpNavigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity :BaseActivity(){
+class MainActivity : BaseActivity(),
+    BottomNavController.NavGraphProvider,
+    BottomNavController.OnNavigationGraphChanged,
+    BottomNavController.OnNavigationReselectedListener
+{
 
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
+        BottomNavController(
+            this,
+            R.id.main_nav_host_fragment,
+            R.id.nav_blog,
+            this,
+            this)
+    }
+
+    override fun getNavGraphId(itemId: Int) = when(itemId){
+        R.id.nav_blog -> {
+            R.navigation.nav_blog
+        }
+        R.id.nav_create_blog -> {
+            R.navigation.nav_create_blog
+        }
+        R.id.nav_account -> {
+            R.navigation.nav_account
+        }
+        else -> {
+            R.navigation.nav_blog
+        }
+    }
+
+    override fun onGraphChange() {
+//        TODO("What needs to happen when the graph changes?")
+    }
+
+    override fun onReselectNavItem(
+        navController: NavController,
+        fragment: Fragment
+    ) = when(fragment){
+
+        is ViewBlogFragment -> {
+            navController.navigate(R.id.action_viewBlogFragment_to_home)
+        }
+
+        is UpdateBlogFragment -> {
+            navController.navigate(R.id.action_updateBlogFragment_to_home)
+        }
+
+        is UpdateAccountFragment -> {
+            navController.navigate(R.id.action_updateAccountFragment_to_home)
+        }
+
+        is ChangePasswordFragment -> {
+            navController.navigate(R.id.action_changePasswordFragment_to_home)
+        }
+
+        else -> {
+            // do nothing
+        }
+    }
+
+    override fun onBackPressed() = bottomNavController.onBackPressed()
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBar()
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController, this)
+        if (savedInstanceState == null) {
+            bottomNavController.onNavigationItemSelected()
+        }
 
         subscribeObservers()
     }
-    
+
     fun subscribeObservers(){
-        sessionManager.cachedToken.observe(this, Observer {authToken ->
-            Log.d(TAG, "subscribeObservers:  subscribeObservers : AuthToken: ${authToken}")
+        sessionManager.cachedToken.observe(this, Observer{ authToken ->
+            Log.d(TAG, "MainActivity, subscribeObservers: ViewState: ${authToken}")
             if(authToken == null || authToken.account_pk == -1 || authToken.token == null){
                 navAuthActivity()
                 finish()
@@ -32,18 +117,22 @@ class MainActivity :BaseActivity(){
         })
     }
 
-    private fun navAuthActivity() {
-        val intent = Intent(this,AuthActivity::class.java)
+    private fun setupActionBar(){
+        setSupportActionBar(tool_bar)
+    }
+
+    private fun navAuthActivity(){
+        val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    override fun displayProgressBar(bool: Boolean) {
+    override fun displayProgressBar(bool: Boolean){
         if(bool){
             progress_bar.visibility = View.VISIBLE
         }
         else{
-            progress_bar.visibility = View.INVISIBLE
+            progress_bar.visibility = View.GONE
         }
     }
 }
